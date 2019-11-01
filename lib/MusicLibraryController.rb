898c77942c2206bd708cc60783adf6baa418f999
Daniel Matney <danieljmatney@gmail.com>
2019-11-01 02:35:46 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -96,7 +96,7 @@ class MusicLibraryController
     puts "Which song number would you like to play?"
     number = gets.chomp.to_i
     list = Song.all.sort {|a, b| a.name <=> b.name}
-    song = list[number]
+    song = list[number + 1]
     puts "Playing #{song.name} by #{song.artist.name}"
   end
   
